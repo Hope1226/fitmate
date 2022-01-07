d897628f4911f7779888c893498f73a653ac43fb
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-07 21:14:31 +0500

    Test out redux store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector} from "react-redux";
+import { storeData } from './redux/workouts/workouts';
+
 
 function App() {
+
+  const tempStore = [
+    {
+      id: 12,
+      name: "Push ups",
+      numSets: 4,
+    }
+  ];
+
+  const globalStore = useSelector(state => state.workoutReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(storeData(tempStore))
+  }, []);
+
+
   return (
     <div className="App">
-      <h1>Main page</h1>
+      <h1>{globalStore[0].name}</h1>
     </div>
   );
 }
