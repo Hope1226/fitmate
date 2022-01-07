80ac487f2811effcda2dfdb8b3b8db55d881fb12
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-07 20:43:25 +0500

    Set up redux store

---
+++ src/redux/workouts/workouts.js
@@ -0,0 +1,21 @@
+const STORE_DATA = 'workoutSrore/workouts/STORE_DATA';
+const intialState = [];
+
+export const storeData = (payload) => (
+  {
+    type: STORE_DATA,
+    payload,
+  }
+);
+
+const reducer = (state = intialState, action) => {
+  switch(action.type) {
+    case STORE_DATA:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
+
