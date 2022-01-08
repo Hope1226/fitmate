a085e44c0b6f66e2b472f0066db0012af1886571
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-09 03:58:23 +0500

    Give bassic style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
-import Workouts from './components/Workouts';
-import Profile from './components/Profile';
-import Dashboard from './components/Dashboard';
+import Workouts from './components/workouts/Workouts';
+import Profile from './components/profile/Profile';
+import Dashboard from './components/dashboard/Dashboard';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App font-sanse">
       <BrowserRouter>
+        <Header />
         <Nav />
         <Routes>
           <Route path="/" exact element={<Dashboard />} />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,15 +5,15 @@ import workoutIcon from '../assets/images/icons/workout-icon.svg';
 import profileIcon from '../assets/images/icons/profile-icon.svg';
 
 const Nav = () => (
-  <nav className="absolute bottom-0 left-0 right-0 flex justify-evenly">
+  <nav className="main-nav">
     <NavLink to="/">
-      <img className="w-16 md:w-32 lg:w-48" src={dashIcon} alt="dashboard icon" />
+      <img className="nav-link" src={dashIcon} alt="dashboard icon" />
     </NavLink>
     <NavLink to="/workouts">
-      <img className="w-16 md:w-32 lg:w-48" src={workoutIcon} alt="workout icon" />
+      <img className="nav-link" src={workoutIcon} alt="workout icon" />
     </NavLink>
     <NavLink to="/profile">
-      <img className="w-16 md:w-32 lg:w-48" src={profileIcon} alt="profile icon" />
+      <img className="nav-link" src={profileIcon} alt="profile icon" />
     </NavLink>
   </nav>
 );
