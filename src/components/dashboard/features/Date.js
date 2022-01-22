6618c8ccce674f9d6b8de84f97203ee1975acd1d
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-22 21:49:47 +0500

    Style calendar component

--- src/components/dashboard/features/Date.js
+++ src/components/dashboard/features/Date.js
@@ -4,14 +4,7 @@ import CalendarSec from './Calendar';
 const Date = () => (
   <div className="dash-children calendar">
     <h2 className="main-headings">Calendar</h2>
-    <div>
-      <div>
-        <h3 className="sub-headings">today</h3>
-        <p>FRI</p>
-      </div>
-      <CalendarSec />
-      <div />
-    </div>
+    <CalendarSec />
   </div>
 );
 
