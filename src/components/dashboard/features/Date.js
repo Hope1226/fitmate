6f317807d6d21cc4bacf03c74e08f45e80d86853
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-22 21:13:22 +0500

    Add calender component

--- src/components/dashboard/features/Date.js
+++ src/components/dashboard/features/Date.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import CalendarSec from './Calendar';
 
 const Date = () => (
   <div className="dash-children calendar">
     <h2 className="main-headings">Calendar</h2>
     <div>
       <div>
-        <h3 className="sub-headings">20 JAN</h3>
+        <h3 className="sub-headings">today</h3>
         <p>FRI</p>
       </div>
+      <CalendarSec />
       <div />
     </div>
   </div>
