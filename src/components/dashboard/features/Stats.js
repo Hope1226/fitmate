9811f2fd9639c669e81aaebf10f02441a0a72992
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-30 15:35:11 +0500

    Refactor stats components

--- src/components/dashboard/features/Stats.js
+++ src/components/dashboard/features/Stats.js
@@ -1,62 +1,39 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 /* eslint-disable no-param-reassign */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const Stats = () => {
-  const [allWorks, setWorks] = useState(12);
-  const [completed, setCompleted] = useState(1);
-  const [percentage, setPercentage] = useState(0);
-
-  const calculatePercantage = () => {
-    const halfCircles = document.querySelectorAll('.half-circle');
-    const halfCirclesTop = document.querySelector('.half-circle-top');
-    const taskComplationDegrees = (360 * completed) / allWorks;
-
-    halfCircles.forEach((el) => {
-      el.style.transform = `rotate(${taskComplationDegrees}deg)`;
-      if (taskComplationDegrees >= 180) {
-        halfCircles[0].style.transform = 'rotate(180deg)';
-        halfCirclesTop.style.opacity = '0';
-      }
-    });
-  };
-
-  useEffect(() => {
-    calculatePercantage();
-  }, []);
-
-  return (
-    <div className="stats-container">
-      <h2 className="main-headings">progress</h2>
-      <div className="prog-data">
-        <div className="prog-chart">
-          <div className="half-circle" />
-          <div className="half-circle" />
-          <div className="half-circle-top" />
-          <div className="prog-circle">
-            {Math.round((completed * 100) / allWorks) }
-            {' '}
-            %
-          </div>
-        </div>
-        <div className="prog-details">
-          <h3 className="sub-headings">Workouts for today</h3>
-          <p>
-            Total:
-            {allWorks}
-          </p>
-          <p>
-            Completed:
-            {completed}
-          </p>
-          <p>
-            Pending:
-            {allWorks - completed}
-          </p>
+const Stats = ({ conpleted, allWorkouts, persantage }) => (
+  <div className="stats-container">
+    <h2 className="main-headings">progress</h2>
+    <div className="prog-data">
+      <div className="prog-chart">
+        <div className="half-circle" />
+        <div className="half-circle" />
+        <div className="half-circle-top" />
+        <div className="prog-circle">
+          {persantage}
+          {' '}
+          %
         </div>
       </div>
+      <div className="prog-details">
+        <h3 className="sub-headings">Workouts for today</h3>
+        <p>
+          Total:
+          {allWorkouts}
+        </p>
+        <p>
+          Completed:
+          {conpleted}
+        </p>
+        <p>
+          Pending:
+          {allWorkouts - conpleted}
+        </p>
+      </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Stats;
