b0c2c26863b63551c0a357889275faafccaf8573
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-30 11:32:38 +0500

    Add activity overlay

--- src/components/dashboard/features/Overlay.js
+++ src/components/dashboard/features/Overlay.js
@@ -2,11 +2,25 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Overlay = ({ toggleClass, closeOverlayFunc }) => (
+const Overlay = ({
+  toggleClass, closeOverlayFunc, condition, imgSrc, actDesc, activityName,
+}) => (
   <div className={`main-overlay ${toggleClass}`}>
-    <h1>Hey I am overlay</h1>
-    <button type="button" className="btns" onClick={closeOverlayFunc}>Stop the session</button>
+    <h1>{activityName}</h1>
+    <div className="action-description">
+      <img src={imgSrc} alt="activity" />
+      <div className="activity-description">
+        <p>{actDesc}</p>
+      </div>
+    </div>
+    <div className="activitiy-actions">
+      <button type="button" className="btns" onClick={closeOverlayFunc}>Stop the session</button>
+      {condition
+        ? <button type="button" className="btns">Next Exercies</button>
+        : <NavLink to="/workouts"><button type="button" className="btns">See the list of workouts</button></NavLink>}
+    </div>
   </div>
 );
 
