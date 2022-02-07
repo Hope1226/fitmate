1ec8f92c8d95f96f1e648779133f34b78da3371c
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-02-07 10:45:56 +0500

    Refactor workout componet

--- src/components/workouts/Workout.js
+++ src/components/workouts/Workout.js
@@ -2,10 +2,26 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
 
-import React from 'react';
+import React, { useState } from 'react';
 import uniqid from 'uniqid';
+import WorkoutOverlay from './WorkoutOverlay';
 
 function Workout({ list }) {
+  const [overlayOpen, setOverlay] = useState(false);
+  const [targetWorkout, setTargetWorkout] = useState({});
+
+  const findWorkout = (id) => {
+    setTargetWorkout(list.filter((el) => el.id === id)[0]);
+    console.log(targetWorkout);
+    setOverlay(true);
+  };
+
+  const closeWorkoutOverlat = () => {
+    setOverlay(false);
+  };
+
+  const nextWorkout = () => console.log('under constructions');
+
   return (
     <div className="workouts">
       {list.map((workout) => (
@@ -14,9 +30,17 @@ function Workout({ list }) {
           <div className="workout-img-container">
             <img src={require(`../../assets/images/workouts/${workout.category}/${workout.category}-${workout.id}.png`)} alt="exercise" />
           </div>
-          <button type="button" className="btns workout-btns">Start this exercise</button>
+          <button type="button" onClick={() => findWorkout(workout.id)} className="btns workout-btns">Start this exercise</button>
         </div>
       ))}
+
+      { overlayOpen ? (
+        <WorkoutOverlay
+          obj={targetWorkout}
+          next={nextWorkout}
+          closeFunc={closeWorkoutOverlat}
+        />
+      ) : null}
     </div>
   );
 }
